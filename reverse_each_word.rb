0c4c904f8699e9a20d4e9b9efec68bdc8246f1f2
Diana Utan <dia_utzi@yahoo.com>
2019-02-14 19:25:26 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   new_array = []
+#   array.each do |word|
+#     string = word.reverse
+#     new_array << string
+#   end
+#   new_array.join(" ")
+# end
+
+def reverse_each_word(string)
+  array = string.split(" ").collect do |word|
+    word.reverse
+  end
+  array.join(" ")
+end
