6e23fb3e38d67c9d0f78e703d41df47f437a6816
Diana Utan <didiutzi@gmail.com>
2020-11-29 17:31:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,12 @@
-def reverse_each_word(string)
-  array = string.split(" ")
-  new_array = []
-  array.each do |word|
-    string = word.reverse
-    new_array << string
-  end
-  new_array.join(" ")
-end
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   new_array = []
+#   array.each do |word|
+#     string = word.reverse
+#     new_array << string
+#   end
+#   new_array.join(" ")
+# end
 
 # def reverse_each_word(string)
 #   array = string.split(" ").collect do |word|
